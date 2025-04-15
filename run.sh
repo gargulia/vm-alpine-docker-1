090fa6a0abdf4c0942d48d9a8c726c52c9f62571
gargulia <artemstefanskij62@gmail.com>
2025-04-15 09:27:56 +0300

    virtual machine startup file
    Bash script for subsequent launches of the virtual machine in Termux.

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~/alpine
+qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 \
+-drive if=pflash,format=raw,read-only,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
+-netdev user,id=n1,hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 \
+-nographic alpine.img
+
+
